8ddce95ccb56f1cbf963ce79016712051252e4f1
Anmol Singh <67062356+anmol109@users.noreply.github.com>
2022-05-09 23:32:55 +0530

    updated added case structure for write data

--- write_data.v
+++ write_data.v
@@ -15,7 +15,22 @@ o_WDATA <= 0;
 end
 
 always @(i_WDATA) begin
+temp_WDATA=0;
 case (WSTRB)
+1'bxxx1:
+temp_WDATA[3:0]=4'b1111;
+1'bxx1x:
+temp_WDATA[7:4]=4'b1111;
+1'bx1xx:
+temp_WDATA[11:8]=4'b1111;
+1'b1xxx:
+temp_WDATA[15:12]=4'b1111;
+default:
+temp_WDATA=0;
+endcase
+i_WDATA=i_WDATA*temp_WDATA;
+end
+
 
 	
 always @(posedge ACLK) begin       //Hand Shaking and addr sending
