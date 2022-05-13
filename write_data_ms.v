bf983f64a687108bac91d1d82e62d2a4c76a5361
Anmol Singh <67062356+anmol109@users.noreply.github.com>
2022-05-13 23:00:54 +0530

    write data channel

--- write_data_ms.v
+++ write_data_ms.v
@@ -1,13 +1,13 @@
-module write_data_master (ACLK, ARESETn,WVALID, WREADY, i_WDATA, o_WDATA, WSTRB);
+module write_data_master (ACLK, ARESETn,i_WVALID,o_WVALID, WREADY, i_WDATA, o_WDATA, i_WSTRB, o_WSTRB);
 input ACLK;
 input ARESETn;
 input i_WVALID;
-output o_WVALID;
+output reg o_WVALID;
 input WREADY;
 input [31:0] i_WDATA;
 output reg [31:0] o_WDATA;
 input [3:0] i_WSTRB;  
-input [3:0] o_WSTRB;         
+output reg [3:0] o_WSTRB;         
 
 reg [31:0] temp_WDATA;
 	
@@ -22,7 +22,7 @@ always @(i_WSTRB) begin
 o_WSTRB=i_WSTRB;
 
 temp_WDATA=32'h0;
-casex (WSTRB)
+casex (i_WSTRB)
 4'b1???: temp_WDATA[31:24]=8'b11111111;
 4'b?1??: temp_WDATA[23:16]=8'b11111111;
 4'b??1?: temp_WDATA[15:8]=8'b11111111;
@@ -49,11 +49,11 @@ end
 endmodule	
 
 
-module write_data_slave (ACLK, ARESETn,WVALID, WREADY, i_WDATA, o_WDATA, WSTRB);
+module write_data_slave (ACLK, ARESETn,WVALID, o_WREADY, i_WREADY, i_WDATA, o_WDATA, WSTRB);
 input ACLK;
 input ARESETn;
 input WVALID;
-output o_WREADY;
+output reg o_WREADY;
 input i_WREADY;
 input [31:0] i_WDATA;
 output reg [31:0] o_WDATA;
@@ -80,13 +80,13 @@ endcase
 temp_WDATA=i_WDATA&temp_WDATA;
 end
 
-always @(i_WVALID) begin       //valid signal high
-		if(i_WVALID)
-		o_WVALID<=1;
-		end
+always @(i_WREADY) begin       //valid signal high
+	if(i_WREADY)
+	o_WREADY<=1;
+	end
 	
 always @(posedge ACLK) begin       //Hand Shaking and data sending
-if (o_WVALID && WREADY) 
+if (o_WREADY && WVALID) 
 o_WDATA <= temp_WDATA;
 		
 else 
