8a918c7aa9106942c77fd8f6d1749824071beeaa
Anmol Singh <67062356+anmol109@users.noreply.github.com>
2022-06-14 13:42:15 +0530

    changes

--- write_address_ms.v
+++ write_address_ms.v
@@ -5,8 +5,8 @@ input [31:0] i_AWADDR;
 input [2:0] AWPROT;
 output [31:0]o_AWADDR;
 
-wire o_AREADY, o_AVALID;
-wire [31:0] w_AWADDR;
+wire o_AREADY=1'b0, o_AVALID=1'b0;
+wire [31:0] w_AWADDR=32'b0;
 
 write_address_master addr_m(ACLK, ARESETn, AWVALID, o_AWVALID, o_AWREADY, i_AWADDR, w_AWADDR, AWPROT);
 write_address_slave addr_s(ACLK, ARESETn, o_AWVALID, AWREADY, o_AWREADY, w_AWADDR, o_AWADDR);
@@ -25,23 +25,30 @@ module write_address_master(ACLK, ARESETn,i_AWVALID, o_AWVALID, AWREADY, i_AWADD
 	output reg [31:0] o_AWADDR;
 	input [2:0] AWPROT;         // Check
 	
-	always @(posedge ARESETn) begin    // RESET
-		o_AWADDR <= 0;
-		o_AWVALID <=0;
-	end
-	
-	always @(i_AWVALID) begin       //Hand Shaking and addr sending
-		if(i_AWVALID)
-		o_AWVALID<=1;
-		end
+		
 
-	always @(posedge ACLK) begin
+	always @(posedge ACLK or posedge ARESETn or i_AWVALID) begin
+		if(ARESETn)
+		begin
+		o_AWADDR <= 32'b0;
+		o_AWVALID <=1'b0;
+		end
+		else if(i_AWVALID)
+		begin
+		if(i_AWVALID)
+		o_AWVALID<=1'b1;
+		else
+		o_AWVALID<=1'b0;
+		end
+		
+		else begin
 
 		if (o_AWVALID && AWREADY) 
 		o_AWADDR <= i_AWADDR;
 		
 		else 
-		o_AWADDR <= 0;
+		o_AWADDR <= 32'b0;
+		end
 	
 	end
 endmodule	
@@ -59,13 +66,13 @@ module write_address_slave(ACLK,ARESETn, AWVALID, i_AWREADY, o_AWREADY, i_AWADDR
 	//input [2:0] AWPROT;         // Check
 	
 	always @(posedge ARESETn) begin    // RESET
-		o_AWADDR <= 0;
-		o_AWREADY <=0;
+		o_AWADDR <= 32'b0;
+		o_AWREADY <=1'b0;
 	end
 	
 	always @(i_AWREADY) begin       //Hand Shaking and addr sending
 		if(i_AWREADY)
-		o_AWREADY<=1;
+		o_AWREADY<=1'b1;
 		end
 
 	always @(posedge ACLK) begin
@@ -73,7 +80,7 @@ module write_address_slave(ACLK,ARESETn, AWVALID, i_AWREADY, o_AWREADY, i_AWADDR
 		o_AWADDR <= i_AWADDR;
 		
 		else 
-		o_AWADDR <= 0;
+		o_AWADDR <= 32'b0;
 	
 	end
 endmodule
